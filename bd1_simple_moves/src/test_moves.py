86cfca52f0847528c04efb6a9f638fb47f19c2df
MoscowskyAnton <moscowskyad@gmail.com>
2020-12-19 23:02:47 +0300

    added legs control service

---
+++ bd1_simple_moves/src/test_moves.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import rospy
+from std_msgs.msg import Float64
+from bd1_simple_moves.srv import SetLegs
+
+class TestServos(object):
+    def __init__(self):
+        rospy.init_node('test_servos')    
+        
+        self.up_r_pub = rospy.Publisher("/up_leg_right_state_controller/command",Float64, queue_size = 10)
+        self.up_l_pub = rospy.Publisher("/up_leg_left_state_controller/command",Float64, queue_size = 10)
+        
+        self.mid_r_pub = rospy.Publisher("/mid_leg_right_state_controller/command",Float64, queue_size = 10)
+        self.mid_l_pub = rospy.Publisher("/mid_leg_left_state_controller/command",Float64, queue_size = 10)
+        
+        self.feet_r_pub = rospy.Publisher("/feet_right_state_controller/command",Float64, queue_size = 10)
+        self.feet_l_pub = rospy.Publisher("/feet_left_state_controller/command",Float64, queue_size = 10)
+        
+        #self.send_cmd(0.5, 0.5, -1., -1, 0.5, 0.5)
+        rospy.Service('~set_legs', SetLegs, self.set_legs_cb)
+    
+    def set_legs_cb(self, req):
+        self.send_cmd(req.up_l, req.up_r, req.mid_l, req.mid_r, req.feet_l, req.feet_r)
+        return []
+    
+    def send_cmd(self, up_l, up_r, mid_l, mid_r, feet_l, feet_r):
+        self.up_r_pub.publish(up_r)
+        self.up_l_pub.publish(up_l)
+        self.mid_r_pub.publish(mid_r)
+        self.mid_l_pub.publish(mid_l)
+        self.feet_l_pub.publish(feet_l)
+        self.feet_r_pub.publish(feet_r)
+    
+    def run(self):
+        rospy.spin()
+    
+    
+    
+if __name__ == '__main__' :
+    ts = TestServos()
+    ts.run()
+    
