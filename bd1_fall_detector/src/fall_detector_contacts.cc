cf0f2778f539f528cb7548dd79397d4e68f8a3fb
Petr Sorokoumov <petr.sorokoumov@gmail.com>
2020-12-28 00:03:09 +0300

    center of pressure calculated but forces are not correct yet

--- bd1_fall_detector/src/fall_detector_contacts.cc
+++ bd1_fall_detector/src/fall_detector_contacts.cc
@@ -3,6 +3,8 @@
  * https://github.com/osrf/gazebo/blob/gazebo11/examples/stand_alone/listener/listener.cc
 */
 
+#include<string>
+
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/gazebo_client.hh>
@@ -10,26 +12,82 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
+#include <geometry_msgs/Point.h>
 
+using std::string;
 
 ros::Publisher fall_pub;
+ros::Publisher center_of_pressure_pub;
 /////////////////////////////////////////////////
 // Function is called everytime a message is received.
 void cb(ConstContactsPtr &_msg)
 {
   std_msgs::Bool fall_msg;
-  if( _msg->contact_size() > 0){      
-      for( size_t i = 0 ; i < _msg->contact_size() ; i++){
-          if( _msg->contact(i).collision1() != "bd1::feet_r_link::feet_r_link_collision" &&
-              _msg->contact(i).collision1() != "bd1::feet_l_link::feet_l_link_collision"){
-              //std::cout << "collision! with" << _msg->contact(i).collision1() << "\n";
-              fall_msg.data = true;
-              fall_pub.publish(fall_msg);
-              return;
-          }
+  geometry_msgs::Point pressure_msg;
+  if( _msg->contact_size() > 0){
+    // fall detection
+    for( size_t i = 0 ; i < _msg->contact_size() ; i++){
+      if( _msg->contact(i).collision1() != "bd1::feet_r_link::feet_r_link_collision" &&
+	  _msg->contact(i).collision1() != "bd1::feet_l_link::feet_l_link_collision"){
+	//std::cout << "collision! with" << _msg->contact(i).collision1() << "\n";
+	fall_msg.data = true;
+	fall_pub.publish(fall_msg);
+	break;
       }
+    }
+    // center of pressure
+    float sum_x = 0;
+    float sum_y = 0;
+    float sum_z = 0;
+    float sum_force_x = 0;
+    float sum_force_y = 0;
+    float sum_force_z = 0;
+    //ROS_ERROR("==============");
+    for( size_t i = 0 ; i < _msg->contact_size() ; i++){
+      string obj1_name = _msg->contact(i).collision1();
+      string obj2_name = _msg->contact(i).collision2();
+      // if ground_plane is first object
+      if (obj1_name.find("ground_plane") != std::string::npos) {
+	for(int j = 0; j < _msg->contact(i).wrench_size(); ++j) {
+	  auto pos = _msg->contact(i).position(j);
+	  auto force1 = _msg->contact(i).wrench(j).body_1_wrench().force();
+	  auto force2 = _msg->contact(i).wrench(j).body_2_wrench().force();
+	  auto force = force1;
+	  //ROS_ERROR("OBJ1 (%.4f %.4f %.4f) (%.4f %.4f %.4f)", pos.x(), pos.y(), pos.z(),
+	  //	    force.x(), force.y(), force.z());
+	  sum_x += pos.x() * force.x();
+	  sum_y += pos.y() * force.y();
+	  sum_z += pos.z() * force.z();
+	  sum_force_x += force.x();
+	  sum_force_y += force.y();
+	  sum_force_z += force.z();
+	}
+      }
+      // if ground_plane is second object
+      if (obj2_name.find("ground_plane") != std::string::npos) {
+	for(int j = 0; j < _msg->contact(i).wrench_size(); ++j) {
+	  auto pos = _msg->contact(i).position(j);
+	  auto force1 = _msg->contact(i).wrench(j).body_1_wrench().force();
+	  auto force2 = _msg->contact(i).wrench(j).body_2_wrench().force();
+	  auto force = force1;
+	  //ROS_ERROR("OBJ2 (%.4f %.4f %.4f) (%.4f %.4f %.4f)", pos.x(), pos.y(), pos.z(),
+	  //	    force.x(), force.y(), force.z());
+	  sum_x += pos.x() * force.x();
+	  sum_y += pos.y() * force.y();
+	  sum_z += pos.z() * force.z();
+	  sum_force_x += force.x();
+	  sum_force_y += force.y();
+	  sum_force_z += force.z();
+	}
+      }
+      
+    }
+    pressure_msg.x = sum_x / sum_force_x;
+    pressure_msg.y = sum_y / sum_force_y;
+    pressure_msg.z = sum_z / sum_force_z;
   }
   fall_pub.publish(fall_msg);
+  center_of_pressure_pub.publish(pressure_msg);
 }
 
 /////////////////////////////////////////////////
@@ -40,6 +98,7 @@ int main(int _argc, char **_argv)
   ros::NodeHandle nh_p("~");
   
   fall_pub = nh_p.advertise<std_msgs::Bool>("fall",1);
+  center_of_pressure_pub = nh_p.advertise<geometry_msgs::Point>("center_of_pressure",1);
  
   // Load gazebo
   gazebo::client::setup(_argc, _argv);
